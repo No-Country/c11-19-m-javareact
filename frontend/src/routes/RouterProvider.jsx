7c13640b8cc76e0de318937a9e0ebb3690cc7487
DruckDeveloper <druckdev@gmail.com>
2023-05-16 08:39:51 -0500

    fix: remove navigate statement ffrom router provider

--- frontend/src/routes/RouterProvider.jsx
+++ frontend/src/routes/RouterProvider.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import { Routes, Route, Navigate, Router } from "react-router-dom";
-import Header from "../components/Header";
-import Home from "../pages/Home";
-import Footer from "../components/Footer";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
 
 export const RouterProvider = () => {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-
-        <Route path="#" element={<Navigate to="/" />} />
+        <Route path='/' element={<Home />} />
       </Routes>
-    </>
-  );
-};
+    </BrowserRouter>
+  )
+}
