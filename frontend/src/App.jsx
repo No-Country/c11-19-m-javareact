ce20164b2a15dc77b31fd6d71cc98988bef0a3b9
DruckDeveloper <druckdev@gmail.com>
2023-05-17 12:38:01 -0500

    chore: import userContext into app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { RouterProvider } from './routes/RouterProvider'
-
+import { UserProvider } from './context/user/UserProvider'
 import { GlobalStyles } from './styled-components/base/GlobalStyles'
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <GlobalStyles />
       <RouterProvider />
-    </>
+    </UserProvider>
   )
 }
 
