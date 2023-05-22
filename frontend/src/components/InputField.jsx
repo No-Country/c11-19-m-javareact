1a5ec349f4c3b06d7f96b1f22cbe86de28b9faf0
bemartin94 <bemartin@hotmail.com.ar>
2023-05-22 11:14:23 -0300

    feat: add input field components

---
+++ frontend/src/components/InputField.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InputField = styled.input`
+  padding: 8px 16px;
+  border: 0.5px solid #808194;
+  border-radius: 30px;
+  position: relative;
+  background: #fff;
+`;
+
+export default InputField;
