34882f35234c8e92d33b74df9162fe36571985a7
Carolinuz <cberges@gmail.com>
2023-05-26 12:14:11 -0300

    chore: add hexagon svg

---
+++ frontend/src/components/icons/Hexagon.jsx
@@ -0,0 +1,23 @@
+const Hexagon = () => {
+  return (
+    <svg width='117' height='128' viewBox='0 0 117 128' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <g filter='url(#filter0_d_847_40044)'>
+        <path d='M20.0034 86.8553C17.5263 85.4268 16 82.7847 16 79.9252V40.0749C16 37.2154 17.5263 34.5733 20.0034 33.1447L54.4012 13.3074C56.8771 11.8796 59.9266 11.881 62.4011 13.3112L96.7151 33.1435C99.1885 34.573 100.712 37.213 100.712 40.0698V79.9302C100.712 82.787 99.1885 85.427 96.715 86.8566L62.401 106.689C59.9266 108.119 56.8771 108.12 54.4012 106.693L20.0034 86.8553Z' fill='#F3F3F3' />
+      </g>
+      <defs>
+        <filter id='filter0_d_847_40044' x='0' y='0.237305' width='116.712' height='127.525' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
+          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+          <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha' />
+          <feOffset dy='4' />
+          <feGaussianBlur stdDeviation='8' />
+          <feComposite in2='hardAlpha' operator='out' />
+          <feColorMatrix type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.16 0' />
+          <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_847_40044' />
+          <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_847_40044' result='shape' />
+        </filter>
+      </defs>
+    </svg>
+  )
+}
+
+export default Hexagon
