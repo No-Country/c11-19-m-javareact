452791060ba1b62cc737b075b82aaea80b9499c5
DruckDeveloper <druckdev@gmail.com>
2023-05-29 08:44:30 -0500

    chore: add footer comopent into principal layout

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
 const FooterContainer = styled.footer`
-  padding: 20px;
+  padding: 4rem 0;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-`;
+`
 
 const FooterText = styled.span`
-  color: #5B7171;
+  color: #5b7171;
   font-size: 14px;
-`;
+`
 
 const Footer = () => {
   return (
@@ -21,7 +21,7 @@ const Footer = () => {
         Copyright © 2023 Upcircle. <p>All rights reserved.</p>
       </FooterText>
     </FooterContainer>
-  );
-};
+  )
+}
 
-export default Footer;
+export { Footer }
