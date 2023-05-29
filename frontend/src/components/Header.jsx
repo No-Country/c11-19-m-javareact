9cf602de97cabe324ba55a5b5521343f60850ff1
DruckDeveloper <druckdev@gmail.com>
2023-05-29 08:13:18 -0500

    chore: add header component into principal layout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-const NavbarContainer = styled.div`
+const HeaderStyled = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0px 24px;
   background: #374444;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
+`
 
 const Logo = styled.img`
   height: 40px;
   max-width: 150px;
-`;
+`
 
-const NavigationBar = () => {
+const Header = () => {
   return (
-    <NavbarContainer>
-      <Logo
-        src='https://res.cloudinary.com/drc41imav/image/upload/v1685218883/UpCircle/LogoD_sz9nqp.svg'
-        alt='Logo'
-      />
+    <HeaderStyled>
+      <Logo src='https://res.cloudinary.com/drc41imav/image/upload/v1685219349/UpCircle/LogoD_azrawy.svg' alt='Logo' />
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Register</a></li>
-        <li><a href="#">Login</a></li>
-
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>Register</a>
+        </li>
+        <li>
+          <a href='#'>Login</a>
+        </li>
       </ul>
-    </NavbarContainer>
-  );
-};
+    </HeaderStyled>
+  )
+}
 
-export default NavigationBar;
+export { Header }
