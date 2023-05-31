f58a0ae8052631e7aa049193c26c1701dca1bbff
DruckDeveloper <druckdev@gmail.com>
2023-05-31 12:33:45 -0500

    chore: add layout to register login pages

--- frontend/src/components/FormLogin.jsx
+++ frontend/src/components/FormLogin.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react'
+import styled from 'styled-components'
 import Boton from './Boton'
 import PasswordField from './PasswordField'
 import EmailInput from './InputEmail'
@@ -8,22 +8,25 @@ import BotonGoogle from './BotonGoogle'
 const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top:10px;
+  margin-top: 10px;
   align-text: center;
-`;
+`
 const RegistroFormulario = () => {
-    return (
-      <FormContainer>
-        <h5 class="form-titulo">Ingresa a Upcircle</h5>
-        <BotonGoogle>Ingresa con Google</BotonGoogle>
-        <p class="form-texto">o ingresa con email</p>
-        <EmailInput />
-        <label>Contraseña</label>
-        <PasswordField /><br/>
-        <Boton>Ingresa</Boton>
-        <a href="#" class="link-login">Olvidaste tu contraseña?</a>
-      </FormContainer>
-    );
-  };
-  
-  export default RegistroFormulario;
+  return (
+    <FormContainer>
+      <h5 class='form-titulo'>Ingresa a Upcircle</h5>
+      <BotonGoogle>Ingresa con Google</BotonGoogle>
+      <p class='form-texto'>o ingresa con email</p>
+      <EmailInput />
+      <label>Contraseña</label>
+      <PasswordField />
+      <br />
+      <Boton>Ingresa</Boton>
+      <a href='#' class='link-login'>
+        Olvidaste tu contraseña?
+      </a>
+    </FormContainer>
+  )
+}
+
+export default RegistroFormulario
