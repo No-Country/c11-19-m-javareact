1381ae86b991fdbc11fc8489436ad0b3e96c393a
ccortesm <ccortesm2000@gmail.com>
2023-06-06 11:00:52 -0500

    feat: add new button carousel and product card

--- frontend/src/components/ProductSlider.jsx
+++ frontend/src/components/ProductSlider.jsx
@@ -1,28 +1,50 @@
 // import { Swiper, SwiperSlide } from 'swiper/react';
 import CardCatalog from "./CardCatalog";
-import { register } from "swiper/element/bundle";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+// import { register } from "swiper/element/bundle";
 // register Swiper custom elements
-register();
+
 // Import Swiper styles
 // import 'swiper/swiper.min.css';
 
 export default () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 980 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 980, min: 650 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 650, min: 416 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 416, min: 0 },
+      items: 1
+    }
+  };
   return (
-    <swiper-container
-      slides-per-view="2"
-      speed="500"
-      loop="true"
-      css-mode="true"
-    >
-      <swiper-slide>
+    <Carousel responsive={responsive}>
+      <div>
         <CardCatalog />
-      </swiper-slide>
-      <swiper-slide>
+      </div>
+      <div>
         <CardCatalog />
-      </swiper-slide>
-      <swiper-slide>
+      </div>
+      <div>
         <CardCatalog />
-      </swiper-slide>
-    </swiper-container>
+      </div>
+      <div>
+        <CardCatalog />
+      </div>
+      <div>
+        <CardCatalog />
+      </div>
+    </Carousel>
   );
 };
