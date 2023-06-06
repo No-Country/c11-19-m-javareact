9b1af5222d76a049bb29e78285e2b21cdf7b9c3a
Dylan Navarro <dylanrubennavarro@hotmail.com>
2023-06-06 13:23:04 -0300

    feat: add CloseButton SVG for Contact Component

---
+++ frontend/src/components/contact/CloseButton.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function CloseButton() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <path
+        d="M12 4L4 12"
+        stroke="#5B7171"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 12L4 4"
+        stroke="#5B7171"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default CloseButton;
