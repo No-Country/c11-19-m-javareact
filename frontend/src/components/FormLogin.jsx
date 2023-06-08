02ac1f2d51ce5730d16d50ed657fbfdaba77e1d3
Carolinuz <cberges@gmail.com>
2023-06-08 11:44:41 -0300

    style: add styles to form login

--- frontend/src/components/FormLogin.jsx
+++ frontend/src/components/FormLogin.jsx
@@ -3,14 +3,55 @@ import { useState } from 'react'
 import { Input } from '../components/Input'
 import { BotonGoogle } from '../components/BotonGoogle'
 import { useAuth } from '../hooks/auth/useAuth'
+import hexagonMobileBg from '../assets/img/hexagon-mobile-bg.svg'
+import hexagonBg from '../assets/img/hexagon-desktop-bg.svg'
 import Boton from './Boton'
+
+const BgForm = styled.div`
+  background-image: url(${hexagonMobileBg});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center center;
+  width: 100%;
+  height: 500px;
+  @media screen and (min-width: 48.0625rem) {
+    background-image: url(${hexagonBg});
+    background-size: cover;
+    background-position: center top;
+    height: 100%;
+  }
+`
 
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   margin-top: 10px;
   text-align: center;
+  margin: 0 auto;
+  font-weight: 800;
+  margin-top: 3.125rem;
+  padding-top: 3.125rem;
+  padding: 3rem 1.5rem 2rem 1.5rem;
+  width: 19.5rem;
+  border-radius: 0.5rem;
+  box-shadow: 0rem 0.25rem 1rem rgba(0, 0, 0, 0.16);
+  background-color: var(--white);
+  @media screen and (min-width: 48.0625rem) {
+    padding: 3.125rem 7.1875rem 3.125rem 7.1875rem;
+    width: 42rem;
+  }
 `
+const FormTitle = styled.h5`
+  width: 16.5rem;
+  text-align: center;
+  color: var(--gray-3);
+  font: var(--heading-small);
+  padding-bottom: 8px;
+  @media screen and (min-width: 48.0625rem) {
+    width: 442px;
+  }
+`
+
 const FormLogin = () => {
   const [form, setForm] = useState({
     email: '',
@@ -30,13 +71,15 @@ const FormLogin = () => {
   }
 
   return (
-    <FormContainer onSubmit={handleSubmit}>
-      <h5 class='form-titulo'>Ingresa a Upcircle</h5>
-      <BotonGoogle>Ingresa con Google</BotonGoogle>
-      <Input label='Email' name='email' type='email' value={form.email} onChange={handleOnChange} placeHolder='XXXXXXXXXXXXXXXXXXXX' />
-      <Input label='Contraseña' name='password' type='password' value={form.password} onChange={handleOnChange} placeHolder='**********' />
-      <Boton>Ingresar</Boton>
-    </FormContainer>
+    <BgForm>
+      <FormContainer onSubmit={handleSubmit}>
+        <FormTitle>Ingresa a Upcircle</FormTitle>
+        <BotonGoogle>Ingresa con Google</BotonGoogle>
+        <Input label='Email' name='email' type='email' value={form.email} onChange={handleOnChange} placeHolder='XXXXXXXXXXXXXXXXXXXX' />
+        <Input label='Contraseña' name='password' type='password' value={form.password} onChange={handleOnChange} placeHolder='**********' />
+        <Boton>Ingresar</Boton>
+      </FormContainer>
+    </BgForm>
   )
 }
 
