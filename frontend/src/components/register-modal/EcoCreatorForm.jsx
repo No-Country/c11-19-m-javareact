7760f2870394676a54e6b48ae3e0983aee3ca8ad
Dylan Navarro <dylanrubennavarro@hotmail.com>
2023-06-09 10:05:17 -0300

    feat: add register functions

--- frontend/src/components/register-modal/EcoCreatorForm.jsx
+++ frontend/src/components/register-modal/EcoCreatorForm.jsx
@@ -1,60 +1,86 @@
-import { Input } from '../Input'
-import { UploadProfileImage } from './UploadProfileImage'
-import { useState } from 'react'
-import { registerEcoCreator } from '../../services/api/user/instances'
-import { useAuth } from '../../hooks/auth/useAuth'
-import Boton from '../Boton'
-import styled from 'styled-components'
+import { Input } from "../Input";
+import { UploadProfileImage } from "./UploadProfileImage";
+import { useState } from "react";
+import { registerEcoCreator } from "../../services/api/user/instances";
+import { useAuth } from "../../hooks/auth/useAuth";
+import Boton from "../Boton";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const FormTitulo = styled.h6`
   text-align: center;
   color: var(--gray-3);
   font: var(--heading-small);
-`
+`;
 
 const FormTituloVerde = styled.h4`
   text-align: center;
   color: var(--green-3);
   font: var(--headline-large);
-`
+`;
 
 const EcoCreatorForm = ({ form, handleOnChange }) => {
-  const [imgUrl, setImgUrl] = useState('')
-  const { userInfo, updateUserInfo } = useAuth()
+  const [imgUrl, setImgUrl] = useState("");
+  const { userInfo, updateUserInfo } = useAuth();
 
   const ecoCreatorModel = {
     firstName: form.name,
     lastName: form.lastName,
     occupation: form.occupation,
     image: imgUrl,
-    email: form.email
-  }
+    email: form.email,
+  };
 
   const handleChangeImage = (imgURL) => {
-    setImgUrl(imgURL[0])
-  }
+    setImgUrl(imgURL[0]);
+  };
 
   const handleSubmit = () => {
     // handleOnChange('', imgUrl)
     registerEcoCreator
-      .post('', ecoCreatorModel)
+      .post("", ecoCreatorModel)
       .then((response) => updateUserInfo(response.data))
-      .catch((error) => console.log(error))
-  }
+      .catch((error) => console.log(error));
+  };
 
-  console.log(userInfo)
+  console.log(userInfo);
 
   return (
     <div>
-      <FormTitulo>Completá tus datos para empezar a transformar el mundo como</FormTitulo>
+      <FormTitulo>
+        Completá tus datos para empezar a transformar el mundo como
+      </FormTitulo>
       <FormTituloVerde>EcoCreador</FormTituloVerde>
-      <Input label='Nombres' type='text' name='name' value={form.name} onChange={handleOnChange} placeHolder='Tu nombre completo ' />
-      <Input label='Apellidos' type='text' name='lastName' value={form.lastName} onChange={handleOnChange} placeHolder='Tu apellido' />
-      <Input label='Ocupación' type='text' name='occupation' value={form.occupation} onChange={handleOnChange} placeHolder='Tu ocupación' />
+      <Input
+        label="Nombres"
+        type="text"
+        name="name"
+        value={form.name}
+        onChange={handleOnChange}
+        placeHolder="Tu nombre completo "
+      />
+      <Input
+        label="Apellidos"
+        type="text"
+        name="lastName"
+        value={form.lastName}
+        onChange={handleOnChange}
+        placeHolder="Tu apellido"
+      />
+      <Input
+        label="Ocupación"
+        type="text"
+        name="occupation"
+        value={form.occupation}
+        onChange={handleOnChange}
+        placeHolder="Tu ocupación"
+      />
       <UploadProfileImage imgValor={handleChangeImage} />
-      <Boton onClick={handleSubmit}>Guardar</Boton>
+      <Boton onClick={handleSubmit}>
+        <Link to="/creator-profile">Guardar</Link>
+      </Boton>
     </div>
-  )
-}
+  );
+};
 
-export { EcoCreatorForm }
+export { EcoCreatorForm };
